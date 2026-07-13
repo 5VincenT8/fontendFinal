58ad1539d8ae63cb63ce7ed5c5fa47029ab81eb1
5VincenT8 <r3794779@gmail.com>
2026-07-12 21:28:57 -0500

    fix 2

--- src/features/auth/services/get-login.js
+++ src/features/auth/services/get-login.js
@@ -1,9 +1,9 @@
 import { API_BASE_URL } from "../../../common/api/api-config";
 
-const endpoint='/login';
+
 export const getLogin = async (username, password) => {
 
-  const response = await fetch(`${API_BASE_URL}${endpoint}`, {
+  const response = await fetch(`${API_BASE_URL}/auth/login`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
